0a2207b009653dfd4573afbc9a6f1a5bff3e178c
Amit Goyal <96512236+JhndaCoder@users.noreply.github.com>
2023-06-21 12:40:47 +0530

    main (#8)
    * Created Header and HomePage
    
    * added logo in navbar
    
    * fixed path issues

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Form from "../../Components/Header/Form/Form";
-import "./Login.css";
+import React from 'react';
+import Form from '../../Components/Form/Form';
+import './Login.css';
 
-const fields = ["Email Id", "Password"];
-const title = "Login";
-const btnValue = "Login";
+const fields = ['Email Id', 'Password'];
+const title = 'Login';
+const btnValue = 'Login';
 
 const Login = () => {
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <Form fields={fields} title={title} btnValue={btnValue}>
-        <a href=''>Forget Password?</a>
+        <a href="">Forget Password?</a>
       </Form>
     </div>
   );
