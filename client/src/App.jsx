ec5ff7700285b2075fb448dbf651a2092f4a3374
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-26 01:52:59 +0530

    Several Pages Made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,30 @@
-import {Fragment} from 'react';
-import Login from '../src/Pages/Login/Login';
-// import SignupApplicant from './Pages/Signup/SignupApplicant';
-import Home from './Pages/Home/Home';
+import { Fragment } from "react";
+import Login from "../src/Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import SignupApplicant from "./Pages/Signup/SignupApplicant";
+import SignupFaculty from "./Pages/Signup/SignupFaculty";
+import SignupRecruiter from "./Pages/Signup/SignupRecruiter";
+import Signup from "./Pages/Signup/Signup";
+import Features from "./Pages/Features/Features";
+import Status from "./Pages/Status/Status";
 
 const App = () => {
   return (
-    <Fragment>
-      {/* <h1 className="title">Placement Portal</h1> */}
-      <Login />
-      {/* <SignupApplicant /> */}
-      {/* <Home /> */}
-    </Fragment>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/features' element={<Features />} exact />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/status' element={<Status />} />
+        <Route path='/signup/applicant' element={<SignupApplicant />} />
+        <Route path='/signup/faculty' element={<SignupFaculty />} />
+        <Route path='/signup/recruiter' element={<SignupRecruiter />} />
+      </Routes>
+    </Router>
   );
 };
 export default App;
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import Form from '../../Components/Form/Form';
-import './Login.css';
+import React from "react";
+import Form from "../../Components/Form/Form";
+import "./Login.css";
+import background from "../../assets/homeBg.svg";
 
-const fields = ['Email Id', 'Password'];
-const title = 'Login';
-const btnValue = 'Login';
+const fields = ["Email Id", "Password"];
+const title = "Login";
+const btnValue = "Login";
 
 const Login = () => {
   return (
-    <div className="form-container">
+    <div className='login-form-container'>
       <Form fields={fields} title={title} btnValue={btnValue}>
-        <a href="">Forget Password?</a>
+        <a href=''>Forget Password?</a>
       </Form>
     </div>
   );
